f630a6c408250d2734d020e5bc0140dbbf97ce68
sjpv <sjpv.milan@hotmail.com>
2025-07-20 23:22:38 -0500

    feat: Uso de fireEvent

--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import CounterApp from "../src/CounterApp";
 
 describe("Prueba de <CounterApp value={100} />", () => {
@@ -12,6 +12,28 @@ describe("Prueba de <CounterApp value={100} />", () => {
         render(<CounterApp value={value} />);
         const numero = screen.getByRole("heading", {level:2}).innerHTML;
         expect(Number(numero)).toBe(value);
+    });
+
+    test('Debe de incrementar el boton +1', () => {        
+        render(<CounterApp value={value} />);        
+        fireEvent.click(screen.getByText("+1"));
+        expect(screen.getByText("101")).toBeTruthy();
+    });
+
+    test('Debe de decrementar el boton -1', () => {        
+        render(<CounterApp value={value} />);        
+        fireEvent.click(screen.getByText("-1"));
+        expect(screen.getByText("99")).toBeTruthy();
+    });
+
+    test('Debe de funcionar el botón de reset', () => {        
+        render(<CounterApp value={value} />);        
+        fireEvent.click(screen.getByText("+1"));
+        fireEvent.click(screen.getByText("+1"));
+        fireEvent.click(screen.getByText("+1"));
+        //fireEvent.click(screen.getByText("Reset"));
+        fireEvent.click(screen.getByRole("button", {name: "btn-reset"}));
+        expect(screen.getByText(value)).toBeTruthy();
     });
 
 });
